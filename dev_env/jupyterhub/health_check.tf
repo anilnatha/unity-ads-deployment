c26bc1a7954a7c47d69a9b6d5fa89cc58078967f
Anil Natha <anilnatha@users.noreply.github.com>
2025-05-27 10:38:22 -0700

    Updated health endpoint metadata for Jupyter

--- dev_env/jupyterhub/health_check.tf
+++ dev_env/jupyterhub/health_check.tf
@@ -3,9 +3,12 @@ resource "aws_ssm_parameter" "jupyter_health_url" {
   name = "/unity/${var.project}/${var.venue}/component/jupyter"
   type = "String"
   value = jsonencode({
+    description    = "The default application-development environment to write and test algorithms before preparing them for scaled execution in the SPS. Pre-configured with authentication tokens and various python libraries to assist in using the various features of the platform."
     healthCheckUrl = "${module.frontend.jupyter_base_url}/${module.frontend.jupyter_base_path}/hub/health"
     landingPageUrl = "${module.frontend.jupyter_base_url}/${module.frontend.jupyter_base_path}/"
+    componentCategory = "development"
     componentName  = "Jupyterhub"
+    componentType  = "ui"
   })
 
   provisioner "local-exec" {
